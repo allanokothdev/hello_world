48ee12319b8d9cd5ec317f65584f06fe832ab3a0
Allan Okoth <allanokothdev@gmail.com>
2023-08-05 01:43:03 +0200

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { $query, $update } from 'azle';
 
 // This is a global variable that is stored on the heap
-let message: string = ''
+let Db = {
+    [key: String]: string,
+};
+
+let db: Db = {};
 
 // Query calls complete quickly because they do not go through consensus
 $query;
-export function getMessage(): string {
-    return message;
+export function get( key: string): string {
+    return db[key];
 }
 
 // Update calls take a few seconds to complete
 // This is because they persist state changes and go through consensus
 $update;
-export function setMessage(newMessage: string): void {
-    message = newMessage; // This change will be persisted
+export function set(key: string, value: string): void {
+    db[key] = value;
 }
